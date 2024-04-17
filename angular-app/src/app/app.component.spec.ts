5d4f361c2188389920279adc95b739b71f725cc4
Mikhail Li <leemisha98@gmail.com>
2024-04-18 00:44:17 +0300

    Change the name

--- angular-app/src/app/app.component.spec.ts
+++ angular-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first-project'`, () => {
+  it(`should have as title 'angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-project');
+    expect(app.title).toEqual('angular-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-first-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-app');
   });
 });
